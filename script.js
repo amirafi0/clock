02a21231dba6cee25eed9f8278e627aadd09d2e2
Ami rafi <kh9961734@gmail.com>
2024-02-07 20:02:34 +0600

    Clock design

--- script.js
+++ script.js
@@ -1,27 +1,16 @@
-let display = document.querySelector('.display');
+let sec = document.querySelector('.sec');
+let min = document.querySelector('.min');
+let hour = document.querySelector('.hour');
 
-let btns = document.querySelectorAll('.btns button');
-for(var i = 0; i < btns.length; i++){
-  var btn = btns[i];
-  
-  btn.addEventListener('click', function(e){
-    var text = e.target.innerText;
-    
-    if( text === '='){
-      var result = eval(display.value);
-      
-      display.value = result;
-    }
-    else if ( text === 'AC'){
-      display.value = '';
-    }
-    else{
-      display.value += text;
-    }
-    
-    
-    
-  });
-  
-  
-}
+setInterval(function(){
+
+var date = new Date();
+var s = date.getSeconds();
+var m = date.getMinutes();
+var h = date.getHours();
+
+sec.innerText = s;
+min.innerText = m;
+hour.innerText = h;
+
+},1000);
